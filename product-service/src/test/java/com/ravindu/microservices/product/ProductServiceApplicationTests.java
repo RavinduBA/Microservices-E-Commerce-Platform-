070c336ce2f08d3adbba7847aff58267eaac9589
RavinduBA <e19004@eng.pdn.ac.lk>
2025-09-16 08:33:59 +0530

    finalized product service
    Added .vscode/settings.json for Java build configuration. Updated product-service test dependencies in pom.xml and modified ProductServiceApplicationTests. Added JVM error and replay logs indicating an out-of-memory error during test execution.

--- product-service/src/test/java/com/ravindu/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/ravindu/microservices/product/ProductServiceApplicationTests.java
@@ -1,15 +1,60 @@
+ 
  package com.ravindu.microservices.product;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+
+import io.restassured.RestAssured;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+    @LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup(){
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+    
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+		String requestBody = """
+				{
+					"name": "iPhone 15",
+					"description": "Latest model of iPhone",
+					"price": 1000
+				}
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/products")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("iPhone 15"))
+				.body("description", Matchers.equalTo("Latest model of iPhone"))
+				.body("price", Matchers.equalTo(1000));				
+			
 	}
 
 }
