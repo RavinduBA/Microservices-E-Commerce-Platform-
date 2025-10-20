4102cfe96f5ea4b917a55092d22604f20511f4d3
RavinduBA <e19004@eng.pdn.ac.lk>
2025-10-20 16:54:09 +0530

    OpenFeign intergrated

---
+++ order-service/src/main/java/com/ravindu/microservices/order/client/InventoryClient.java
@@ -0,0 +1,16 @@
+package com.ravindu.microservices.order.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(value = "inventory", url = "http://localhost:8082")
+public interface InventoryClient {
+ 
+    @RequestMapping(method = RequestMethod.GET, value="/api/inventory")
+    boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity );
+    
+
+
+}
--- order-service/src/main/java/com/ravindu/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/ravindu/microservices/order/service/OrderService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import com.ravindu.microservices.order.dto.OrderRequest;
 import com.ravindu.microservices.order.model.Order;
 import com.ravindu.microservices.order.repository.OrderRepository;
+import com.ravindu.microservices.order.client.InventoryClient;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,15 +15,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class OrderService {
 
+    private final InventoryClient inventoryClient;
     private final OrderRepository orderRepository;
 
     public void placeOrder(OrderRequest orderRequest) {
+
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+       if (isProductInStock){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         order.setPrice(orderRequest.price());
         order.setSkuCode(orderRequest.skuCode());
         order.setQuantity(orderRequest.quantity());
         orderRepository.save(order);
+       } else {
+         throw new RuntimeException("Product with SkuCode" + orderRequest.skuCode() + " is not in stock");
+
+       }
 
     }
 
